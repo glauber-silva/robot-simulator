78dc199e8bb89bbba36784ba546c716cd25603ae
Glauber Silva <glauber.lucio.silva@gmail.com>
2020-09-28 20:03:35 -0300

    - add function to validate position and direction

--- lib/robot_simulator.ex
+++ lib/robot_simulator.ex
@@ -1,18 +1,24 @@
 defmodule RobotSimulator do
-  @moduledoc """
-  Documentation for `RobotSimulator`.
-  """
+
+
+  @valid_directions [:north, :south, :east, :west]
 
   @doc """
-  Hello world.
+  Create a Robot Simulator given an initial direction and position.
 
-  ## Examples
-
-      iex> RobotSimulator.hello()
-      :world
-
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
-  def hello do
-    :world
+  def create(direction, position) when valid_direction(direction) and valid_position(position)
+    %RobotSimulator{
+      direction: direction,
+      position: position
+    }
   end
+
+  defp valid_direction(direction) when direction in @valid_directions, do: :ok
+  defp valid_direction(_), do: {:error, "invalid direction"}
+
+  defp valid_position({x, y}) when is_integer(x) and is_integer(y), do: :ok
+  defp valid_position(_), do: {:error, "invalid position"}
+
 end
